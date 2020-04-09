bbee05841265f4472b683fd7660df17d0ac01797
Nobuyuki Takahiro <takahiro@kumihan.com>
2020-04-09 11:49:24 +0900

    learn one to one population. sample: repl.js

--- models/course.js
+++ models/course.js
@@ -5,15 +5,14 @@ const courseSchema = new Schema({
   title: {
     type: String,
     reqired: true,
-    unique: true
+    unique: true,
   },
-  // description: {
-  //   type: String,
-  //   required: true,
-  // },
-  participant: [{
-    type: Schema.Types.ObjectId, ref: "Member"
-  }]
+  teacher: { type: Schema.Types.ObjectId, ref: "Member" },        // 『講座』の『先生』は一人
+  participants: [{ type: Schema.Types.ObjectId, ref: "Member" }], // 『講座』の『受講生（生徒）』は複数
+  description: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Course", courseSchema);
