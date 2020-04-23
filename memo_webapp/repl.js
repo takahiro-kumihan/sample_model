e8fd1be0dd91fa285131b75e1abdf51847cf412b
Nobuyuki Takahiro <takahiro@kumihan.com>
2020-04-23 14:18:10 +0900

    bcryptでパスワードのハッシュ化とログイン実装する。

--- memo_webapp/repl.js
+++ memo_webapp/repl.js
@@ -1,90 +1,105 @@
 "use strict";
 
-const mongoose = require("mongoose"),
-      Course = require("./models/course"),
-      Subscriber = require("./models/subscriber"),
-      User = require("./models/user");
+// // パスワードのハッシュ化
+// const bcrypt = require("bcrypt");
+// const pw = "takahiro";
+// let hashed_pw = bcrypt.hashSync(pw, 10);
+// console.log(hashed_pw);
 
-// インスタンスの中、代入されて以降の範囲で使えるローカル変数を
-// インスタンスの外側で定義する。
-var tmpCourse, tmpSubscriber, tmpUser;
+// ハッシュ化されたパスワードとの照合
+const bcrypt = require("bcrypt");
+const pw = "takahiro";
+let hashed_pw = bcrypt.hashSync(pw, 10);
 
-mongoose.connect(
-  "mongodb://localhost:27017/recipi_db",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }
-);
+bcrypt.compareSync("takahiro", hashed_pw);
+// bcrypt.compareSync("takahashi", hashed_pw);
 
-mongoose.Promise = global.Promise;
 
-// /////////////////////////////////////////////
-// // create Course model
+// const mongoose = require("mongoose"),
+//       Course = require("./models/course"),
+//       Subscriber = require("./models/subscriber"),
+//       User = require("./models/user");
 
-let course_sets = [
-  {
-    title: "トマトのソースパスタ",
-    cost: 800,
-    discription: "無農薬トマトのソースパスタ。",
-    items: ["トマト", "玉ねぎ"],
-    zip_code: 1234
-  },
-  {
-    title: "とんかつ丼",
-    cost: 700,
-    discription: "黒豚のとんかつ。",
-    items: ["黒豚肉", "玉ねぎ", "卵"],
-    zip_code: 2345
-  },
-  {
-    title: "揚げそば",
-    cost: 900,
-    discription: "広東風野菜たっぷりの揚げそば。",
-    items: ["白菜", "そば", "玉ねぎ"],
-    zip_code: 3456
-  }
-],
-commands = [],
-query = {
-  $and: [
-          { "title": "トマトのソースパスタ" },
-          { "title": "揚げそば" },
-        ]
-};
+// // インスタンスの中、代入されて以降の範囲で使えるローカル変数を
+// // インスタンスの外側で定義する。
+// var tmpCourse, tmpSubscriber, tmpUser;
 
-Course.deleteMany()
-  .exec()
-  .then(() => {
-    console.log("Course model collection delete!")
-  })
-  .then(() => {
-    course_sets.forEach(cs => {
-      commands.push(Course.create({
-        title: cs.title,
-        cost: cs.cost,
-        discription: cs.discription,
-        items: cs.items,
-        zip_code: cs.zip_code
-      }))
-    });
-    Promise.all(commands)
-    .then(rt => {
-      console.log(JSON.stringify(rt));
-      mongoose.connection.close();
-    })
-  })
+// mongoose.connect(
+//   "mongodb://localhost:27017/recipi_db",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+//   }
+// );
+
+// mongoose.Promise = global.Promise;
+
+// // /////////////////////////////////////////////
+// // // create Course model
+
+// let course_sets = [
+//   {
+//     title: "トマトのソースパスタ",
+//     cost: 800,
+//     discription: "無農薬トマトのソースパスタ。",
+//     items: ["トマト", "玉ねぎ"],
+//     zip_code: 1234
+//   },
+//   {
+//     title: "とんかつ丼",
+//     cost: 700,
+//     discription: "黒豚のとんかつ。",
+//     items: ["黒豚肉", "玉ねぎ", "卵"],
+//     zip_code: 2345
+//   },
+//   {
+//     title: "揚げそば",
+//     cost: 900,
+//     discription: "広東風野菜たっぷりの揚げそば。",
+//     items: ["白菜", "そば", "玉ねぎ"],
+//     zip_code: 3456
+//   }
+// ],
+// commands = [],
+// query = {
+//   $and: [
+//           { "title": "トマトのソースパスタ" },
+//           { "title": "揚げそば" },
+//         ]
+// };
+
+// Course.deleteMany()
+//   .exec()
+//   .then(() => {
+//     console.log("Course model collection delete!")
+//   })
+//   .then(() => {
+//     course_sets.forEach(cs => {
+//       commands.push(Course.create({
+//         title: cs.title,
+//         cost: cs.cost,
+//         discription: cs.discription,
+//         items: cs.items,
+//         zip_code: cs.zip_code
+//       }))
+//     });
+//     Promise.all(commands)
+//     .then(rt => {
+//       console.log(JSON.stringify(rt));
+//       mongoose.connection.close();
+//     })
+//   })
   // // ここも非同期の罠にひっかかる。
   // .then(() => {
   //   Course.find(query, function (err, data) {
   //     console.log(`${ data }\n\n\n\n\n`)
   //   });
   // })
-  .catch(err => {
-    console.log(err)
-  });
+  // .catch(err => {
+  //   console.log(err)
+  // });
 
 
 
