2cb79324f7e4bd8202f288f51773cda1fbf43ef3
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-04-27 15:02:39 +0900

    where are query, url, bosy, params?

--- memo_webapp/main.js
+++ memo_webapp/main.js
@@ -4,8 +4,13 @@ const express = require("express"),
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.send("Hello, Node.js!");
+  // res.send("Hello, Node.js!");
+  // res.send(req.query);
+  // res.send(req.url);
+  // res.send(req.body);
+  res.send(req.params);
 });
+
 
 app.listen(app.get("port"), () => {
   console.log(`サーバーはhttp://localhost:${app.get("port")}で起動しています。`);
